0be0329eeff857a14243d5db24cc1e81f261ea96
raf-nr <nurmuhametovrafik04@gmail.com>
2024-03-13 22:52:55 +0300

    Update parameters

--- first_task/param.c
+++ first_task/param.c
@@ -2,9 +2,9 @@
 
 #define BLOCK_SIZE 64
 #define EPS 0.1
-#define N_ARRAY {100}
-#define THREADS_ARRAY {1, 2, 4, 8, 12, 16}
+#define N_ARRAY {50, 100, 200, 300, 400, 500, 1000, 2000, 3000, 4000}
+#define THREADS_ARRAY {1, 2, 4, 8, 10}
 
-double d_kx3_p_2ky3(double x, double y) { return 6000 * x + 12000 * y; }
+double f_declr (double x, double y) { return 6000 * x + 12000 * y; }
 
-double kx3_p_2ky3(double x, double y) { return 1000 * pow(x, 3) + 2000 * pow(y, 3); }
+double g_declr (double x, double y) { return 1000 * pow(x, 3) + 2000 * pow(y, 3); }
